778902ea811787f873e18000788fb9b10c367c83
joamthestalker <80638883+joamthestalker@users.noreply.github.com>
2022-02-28 21:12:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "!", description = "Bot de Titouan")
+
+@bot.event
+async def on_ready():
+	print("Ready !")
+
+
+
+@bot.command()
+async def ban(ctx, user : discord.User, *reason):
+	reason = " ".join(reason)
+	await ctx.guild.ban(user, reason = reason)
+	await ctx.send(f"{user} à été ban pour la raison suivante : {reason}.")
+
+@bot.command()
+async def unban(ctx, user, *reason):
+	reason = " ".join(reason)
+	userName, userId = user.split("#")
+	bannedUsers = await ctx.guild.bans()
+	for i in bannedUsers:
+		if i.user.name == userName and i.user.discriminator == userId:
+			await ctx.guild.unban(i.user, reason = reason)
+			await ctx.send(f"{user} à été unban.")
+			return
+
+	await ctx.send(f"L'utilisateur {user} n'est pas dans la liste des bans")
+
+@bot.command()
+async def kick(ctx, user : discord.User, *reason):
+	reason = " ".join(reason)
+	await ctx.guild.kick(user, reason = reason)
+	await ctx.send(f"{user} à été kick pour la raison suivante : {reason}.")
+
+
+@bot.command()
+async def clear(ctx, nombre : int):
+	messages = await ctx.channel.history(limit = nombre + 1).flatten()
+	for message in messages:
+		await message.delete()
+
+
+bot.run("OTQwMTgxODI5OTkzMDAwOTYw.YgDqkA.QklChFlhIxU8R2PuKEE0R6w70Vc")
